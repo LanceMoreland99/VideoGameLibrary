cd9a047f28961c93e9d34cd21f48573bb9476d8a
Lance Moreland <lmoreland1899@gmail.com>
2018-02-14 10:58:41 -0500

    added menu options 1-5

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -1,8 +1,79 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
 public class Library {
 
-    protected String addGame;
-    //Code goes here to add game where ever we are saving game things to
+    public final Menu menu;
+    private List<Game> gameLibrary = new ArrayList<Game>();
+    private List<Game> checkedOutGames = new ArrayList<Game>();
+    private List<Game> checkedInGames = new ArrayList<Game>();
+
+    private SimpleDateFormat dateFormat = new SimpleDateFormat( "MM/dd/yy, HH:mm:ss");
+
+
+
+    public Library (Menu menu) {
+        this.menu = menu;
+    }
+
+    protected void addGame(Game game) {
+        //Code goes here to add game where ever we are saving game things to
+
+        gameLibrary.add(game);
+        Menu menu = new Menu();
+        menu.startMenu();
+    }
+
+    protected void removeGame(int gameIndex) {
+        //Code to remove game goes here
+
+        gameIndex -= gameIndex;
+        gameLibrary.remove(gameIndex);
+        System.out.println("You have removed a game from your library. ");
+        menu.startMenu();
+    }
+
+    protected void checkOutGames(int gameIndex) {
+        //code for checkedOutGames goes here
+
+        gameIndex -= gameIndex;
+        Game game = gameLibrary.get(gameIndex);
+        checkedOutGames.add(game);
+        Calendar calendar = Calendar.getInstance();
+
+        System.out.println("You checked out this game" + dateFormat.format(calendar.getTime()));
+
+        calendar.add(Calendar.DAY_OF_YEAR, 3);
+
+        System.out.println("it is due back on : " + dateFormat.format(calendar.getTime()));
+
+        game.setDueDate(dateFormat.format(calendar.getTime()));
+
+    }
+
+    protected void checkInGames(int gameIndex) {
+
+        //code for
+
+        for (Game game :checkedOutGames) {
+            System.out.println(game.getTitle());
+        }
+
+    }
+
+    protected void listGames() {
+
+        // Create a foreach loop to iterate through the list of games in the library.  Display those items to the user
+
+        for (Game game : gameLibrary) {
+            System.out.println(game.getTitle());
+        }
+        //Go back to the menu
+        menu.startMenu();
+    }
 
 }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,11 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
 
     private Scanner input = new Scanner(System.in);
+    private Library library = new Library(this);
 
     //User to prompt user library options
 
@@ -23,12 +25,24 @@ public class Menu {
 
             switch (input.nextInt()) {
                 case 1:
+                    System.out.println("You have chosen to add a new game to your library." +
+                            "what is the name of the game?");
+                    input.nextLine();
+                    Game game = new Game(input.nextLine());
+                    System.out.println("You have added:" + game.getTitle() + " " + "to your library.");
+                    library.addGame(game);
+
                     //Add game
                     break;
                 case 2:
+                    System.out.println("You have chosen to remove a game from the library" +
+                            "please chose from one of the following by number.");
+                    library.removeGame(input.nextInt());
+                    System.out.println();
                     //Remove game
                     break;
                 case 3:
+                    library.listGames();
                     //View Library
                     break;
                 case 4:
@@ -44,12 +58,12 @@ public class Menu {
                     //Exit game
                     break;
                 default:
-                    //Prompt user to pick correct loop back to menu
+                    //Prompt user to pick correct number, loop back to menu.
                     break;
             }
         }catch (InputMismatchException ime){
             input.nextLine();
-            System.out.println("Select a number form 1 - 7");
+            System.out.println("Select a number from 1 - 7");
             startMenu();
         }
     }
